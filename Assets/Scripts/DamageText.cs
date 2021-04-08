8ad1d36d8cbea07c58fa7e4cc85c2a2504269ca8
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-04-08 16:24:59 -0300

    Arrumando sistema de exibição de dano.

---
+++ Assets/Scripts/DamageText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageText : MonoBehaviour
+{
+    [SerializeField]
+    private Text text;
+    
+    // Start is called before the first frame update
+    public void Build(int damage, Color color)
+    {
+        Destroy(gameObject, .5f);
+        text.text = damage.ToString();
+        text.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * (Time.deltaTime * 5f);
+    }
+}
--- Assets/Scripts/DamageTextManager.cs
+++ Assets/Scripts/DamageTextManager.cs
@@ -5,11 +5,6 @@ using UnityEngine.UI;
 
 public class DamageTextManager : MonoBehaviour
 {
-    private class DamageText
-    {
-        public float lifeTime;
-        public Text text;
-    }
 
     #region Singleton
     public static DamageTextManager Instance { get; private set; }
@@ -21,11 +16,10 @@ public class DamageTextManager : MonoBehaviour
     #endregion
 
     [SerializeField]
-    private Text textPrefab;
+    private DamageText damageText;
     [SerializeField]
     private Transform canvas;
 
-    private List<DamageText> texts = new List<DamageText>();
     private Camera mainCamera;
 
     private void Start()
@@ -33,35 +27,9 @@ public class DamageTextManager : MonoBehaviour
         mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        for(int i = 0; i < texts.Count; i++)
-        {
-            var txt = texts[i];
-            txt.lifeTime -= Time.deltaTime;
-            txt.text.transform.position += Vector3.up * Time.deltaTime * 5f;
-
-            if(txt.lifeTime <= 0f)
-            {
-                texts.RemoveAt(i);
-                Destroy(txt.text.gameObject);
-                i--;
-            }
-        }
-    }
-
     public void AddDamageText(Vector3 position, int value, bool critical)
     {
-        DamageText damageText = new DamageText();
-        var text = Instantiate(textPrefab, position, Quaternion.identity, canvas).GetComponent<Text>();
-        text.color = critical ? Color.yellow : Color.red;
-        text.text = value.ToString();
-
-        damageText.text = text;
-        damageText.lifeTime = .5f;
-        damageText.text.transform.position += Vector3.right * Random.Range(-.5f, .5f);
-
-        texts.Add(damageText);
+        var obj = Instantiate(damageText, position + Vector3.right * Random.Range(-.5f, .5f), Quaternion.identity, canvas);
+        obj.GetComponent<DamageText>().Build(value, critical ? Color.yellow : Color.red);
     }
 }
