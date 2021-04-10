2eb54f4426e34abc6c7f46f140990f4fd985f2d5
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-04-10 20:20:28 -0300

    Efeito de tela ao receber dano.

---
+++ Assets/Scripts/DamageScreenEffect.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CharacterState))]
+public class DamageScreenEffect : MonoBehaviour
+{
+    [SerializeField]
+    private Image damageScreen;
+
+    private float duration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var cs = GetComponent<CharacterState>();
+        cs.OnDamage += OnDamage;
+        enabled = false;
+    }
+
+    private void OnDamage(DamageEvent damage)
+    {
+        enabled = true;
+        duration = 1f;
+        var color = damageScreen.color;
+        color.a = 1f;
+        damageScreen.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        duration -= Time.deltaTime;
+        
+        var color = damageScreen.color;
+        color.a = duration * .4f;
+        damageScreen.color = color;
+
+        if(duration <= 0f)
+        {
+            enabled = false;
+        }
+    }
+}
