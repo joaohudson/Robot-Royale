f66f573fcec227cfc92fb7462f26b063936023d7
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-04-11 14:46:57 -0300

    Efeito de escala na animação.

--- Assets/Scripts/DamageText.cs
+++ Assets/Scripts/DamageText.cs
@@ -8,17 +8,27 @@ public class DamageText : MonoBehaviour
     [SerializeField]
     private Text text;
 
+    private float size;
+    private float duration;
+    private Vector3 baseScale;
+
     // Start is called before the first frame update
     public void Build(int damage, Color color)
     {
         Destroy(gameObject, .5f);
         text.text = damage.ToString();
         text.color = color;
+        size = 1f;
+        duration = 1f;
+        baseScale = text.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.up * (Time.deltaTime * 5f);
+        text.transform.localScale = baseScale * size;
+        duration -= Time.deltaTime * 2f;
+        size = Mathf.Sqrt(duration);
     }
 }
