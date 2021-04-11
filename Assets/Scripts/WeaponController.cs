fdfd08788a09dee71bf7f9e319c478b4656a03c0
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-04-11 14:48:36 -0300

    Revert "Revert "Correção na label de munição.""
    This reverts commit b260b196a66d4186802884709d6fe8c2c6494c1e.

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -38,6 +38,7 @@ public class WeaponController : MonoBehaviour
     private float interval = 0f;
     private float reloadTime = 0f;
     private int ammo;
+    private float disableLight;
 
     private void Start()
     {
@@ -59,6 +60,22 @@ public class WeaponController : MonoBehaviour
                 Ammo = projectile.count;
             }
         }
+    }
+
+    /// <summary>
+    /// Ponto de onde o disparo é efetuado.
+    /// </summary>
+    public Transform FirePoint
+    {
+        get => firePoint;
+    }
+
+    /// <summary>
+    /// O projétil desta arma.
+    /// </summary>
+    public ProjectileInfo Projectile
+    {
+        get => projectile;
     }
 
     /// <summary>
