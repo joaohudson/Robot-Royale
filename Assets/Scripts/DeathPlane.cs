9182f9db649e9da92af402de9c4bebc986ae5370
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-04-11 15:33:18 -0300

    Adição de morte ao cair do mapa.

---
+++ Assets/Scripts/DeathPlane.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var state = other.GetComponent<CharacterState>();
+
+        if(state != null)
+        {
+            state.Kill();
+        }
+    }
+}
