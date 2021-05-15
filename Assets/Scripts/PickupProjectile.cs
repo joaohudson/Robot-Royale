14121558144fe33a0a5edac39fd9724de7405537
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-05-15 20:12:48 -0300

    Adição de troca de projéteis(coleta).

---
+++ Assets/Scripts/PickupProjectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupProjectile : MonoBehaviour
+{
+    [SerializeField]
+    private ProjectileInfo projectile;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<WeaponController>().Projectile = projectile;
+            Destroy(gameObject);
+        }
+    }
+}
