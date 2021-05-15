ed0aa1396c7af29e21e629d6db0bad7728296f12
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-05-15 15:04:37 -0300

    Atualização no efeito de explosão.

---
+++ Assets/Scripts/MeshColor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshColor : MonoBehaviour
+{
+    [SerializeField]
+    private Color color;
+
+    private MaterialPropertyBlock propertie;
+    private MeshRenderer meshRenderer;
+
+    private void OnValidate()
+    {
+        Start();
+        Update();
+    }
+
+    void Start()
+    {
+        propertie = new MaterialPropertyBlock();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propertie.SetColor("_Color", color);
+        meshRenderer.SetPropertyBlock(propertie);
+    }
+}
