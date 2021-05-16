7c63a2b08c2b17f987efe5389d52a1be481adf5c
joaohudson <joaoliveira@cc.ci.ufpb.br>
2021-05-15 22:07:20 -0300

    Coleta de projéteis.

--- Assets/Scripts/PickupProjectile.cs
+++ Assets/Scripts/PickupProjectile.cs
@@ -6,12 +6,15 @@ public class PickupProjectile : MonoBehaviour
 {
     [SerializeField]
     private ProjectileInfo projectile;
+    [SerializeField]
+    private AudioClip pickupSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<WeaponController>().Projectile = projectile;
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position);
             Destroy(gameObject);
         }
     }
